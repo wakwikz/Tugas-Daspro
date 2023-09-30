31d2632caaa677677e76e1f2e515d0e1d291f9c5
wakwikz <haqibaihaqi471@gmail.com>
2023-09-30 22:23:09 +0700

    Jobsheet 5

---
+++ PemilihanPercobaan102.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PemilihanPercobaan102 {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        int angka;
+
+        System.out.println("Masukkan angka: ");
+        angka =  input.nextInt();
+
+        String result = (angka%2==0) ? "Angka "+angka+" bilangan genap" : "Angka "+angka+" bilangan ganjil";
+        System.out.println(result);
+    }
+    }
