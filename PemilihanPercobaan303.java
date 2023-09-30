6a23545e6aad97d5e590fd8d74c093b608206767
wakwikz <haqibaihaqi471@gmail.com>
2023-10-01 00:09:28 +0700

    Jobsheet 5

---
+++ PemilihanPercobaan303.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class PemilihanPercobaan303 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.println("Masukkan angka pertama: ");
+        angka1 = input.nextDouble();
+        System.out.println("Masukkan angka kedua: ");
+        angka2 = input.nextDouble();
+        System.out.println("Masukkan operator (+ - * /): ");
+        operator = input.next().charAt(0);
+
+        switch (operator){
+            case '+':
+            hasil = angka1 + angka2;
+            System.out.println(angka1 + " + " + angka2 + "=" + hasil);
+    
+            case '-':
+            hasil = angka1 - angka2;
+            System.out.println(angka1 + " - " + angka2 + "=" + hasil);
+            break;
+            case '*':
+            hasil = angka1 * angka2;
+            System.out.println(angka1 + " * " + angka2 + "=" + hasil);
+            break;
+            case '/':
+            hasil = angka1 / angka2;
+            System.out.println(angka1 + " / " + angka2 + "=" + hasil);
+            break;
+        }
+    }
+}
