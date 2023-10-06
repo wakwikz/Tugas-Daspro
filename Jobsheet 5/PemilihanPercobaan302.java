50232fa19b42d5957ae71ebbd931fb58aad61883
wakwikz <haqibaihaqi471@gmail.com>
2023-10-06 13:54:11 +0700

    Jobsheet 5

--- Jobsheet 5/PemilihanPercobaan302.java
+++ Jobsheet 5/PemilihanPercobaan302.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PemilihanPercobaan303 {
+public class PemilihanPercobaan302 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
