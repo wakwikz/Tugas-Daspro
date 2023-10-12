d17030def5b20d41d8625afcbde55bfeb2cb3533
wakwikz <haqibaihaqi471@gmail.com>
2023-10-12 20:56:08 +0700

    Lab  01 Kamis

---
+++ OCW UI Lab/DDP2_Lab_01/GanjilGenap.java
@@ -0,0 +1,20 @@
+package DDP2_Lab_01;
+import java.util.Scanner;
+public class GanjilGenap{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int nomorPlat;
+
+        System.out.println("Masukkan nomor plat anda: ");
+        nomorPlat = input.nextInt();
+
+        if (nomorPlat > 9999 || nomorPlat < 0) {
+            System.out.println("Nomor plat anda salah");
+        } else if (nomorPlat % 2 == 0) {
+            System.out.println("Nomor plat anda genap");
+        } else {
+            System.out.println("Nomor plat anda ganjil");
+        }
+    }
+}
---
+++ OCW UI Lab/DDP2_Lab_01/PembagianManual.java
@@ -0,0 +1,24 @@
+package DDP2_Lab_01;
+import java.util.Scanner;
+public class PembagianManual {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int angka, angkaAwal, pembagi, hasil=0, sisa=0;
+  
+        System.out.println("Masukkan angka yang ingin anda bagi: ");
+        angka = input.nextInt();
+        System.out.println("Masukkan pembagi angka: ");
+        pembagi = input.nextInt();
+        
+        angkaAwal = angka;
+
+        while ((angka - pembagi) >= 0) {
+            angka -= pembagi;
+            sisa = angka;
+            hasil++;
+        }
+
+        System.out.println("Hasil pembagian " + angkaAwal + " dengan " + pembagi + " adalah " + hasil + " dengan sisa " + sisa);
+    }
+}
